d92fe823055beb7a7d252dd3bd81702e80ae96a9
eswar <neelaveni.eswar04@gmail.com>
2022-03-11 13:20:36 +0530

    displaying dice number

---
+++ dice.sh
@@ -0,0 +1,12 @@
+dice_num=$(((RANDOM%6)+1))
+$dice_number
+
+echo "One!::$dice_num"
+echo "Two!::$dice_num"
+echo "Three!::$dice_num"
+echo "Four!::$dice_num"
+echo "Five!::$dice_num"
+echo "Six!::$dice_num"
+echo "Nothing between one to six"
+
+
